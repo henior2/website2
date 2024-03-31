31d5bd3978da969cb8cf4618705144baf45bf29f
Henryk <63795118+henior2@users.noreply.github.com>
2024-03-31 17:33:09 +0200

    add gif change interval and migrate to pnpm

--- src/components/FunniGif.vue
+++ src/components/FunniGif.vue
@@ -3,25 +3,45 @@ import gifs from '@/content/gifs.json'
 import { ref, onMounted } from 'vue'
 
 const gif = ref("")
+
+const isLoaded= ref(false)
+
+const timeout = ref(0)
+
 const randomGif = () => {
+	clearTimeout(timeout.value)
 	const prevGif = gif.value
 	while (gif.value === prevGif) {
 		gif.value = gifs[Math.floor(Math.random() * gifs.length)]
 	}
+	isLoaded.value = false
+	timeout.value = setTimeout(() => {
+		randomGif()
+	}, 15000)
 }
+
 onMounted(randomGif)
 
 </script>
 
 <template>
-	<img :src="gif" :alt="`funny gif from ${gif}`" height="200" class="gif" @click="randomGif">
+	<img :src="gif" :alt="`funny gif from ${gif}`" height="200" class="gif" @click="randomGif" @load="isLoaded=true" v-show="isLoaded">
+	<div class="loading" v-show="!isLoaded"></div>
 
 </template>
 
 <style scoped>
-.gif {
-	margin: 0 auto;
+
+.gif, .loading {
 	display: block;
+	margin: 2rem auto 0 auto;
+}
+
+.gif {
 	cursor: pointer;
 }
+.loading {
+	height: 200px;
+}
+
 </style>
